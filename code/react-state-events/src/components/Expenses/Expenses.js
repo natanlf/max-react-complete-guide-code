c4def5b519a6c3bf06495db0355dccd9a97d48ca
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-01-13 23:01:18 -0300

    expense filter assignment solution

--- code/react-state-events/src/components/Expenses/Expenses.js
+++ code/react-state-events/src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from "../UI/Card";
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2023');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear)
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
