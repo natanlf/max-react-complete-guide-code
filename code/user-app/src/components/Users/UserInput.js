e8657fd7485944e26e7f3328001aa6478a0bc0f4
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-01-20 20:22:54 -0300

    styled components

--- code/user-app/src/components/Users/UserInput.js
+++ code/user-app/src/components/Users/UserInput.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './UserInput.css';
+import styles from './UserInput.module.css';
 import Card from "../../UI/Card";
 import Button from "../../UI/Button";
 
@@ -8,13 +8,21 @@ const UserInput = props => {
 
     const [enteredName, setEnteredName] = useState('');
     const [enteredAge, setAge] = useState('');
+    const [nameIsValid, setNameIsValid] = useState(true);
+    const [ageIsValid, setAgeIsValid] = useState(true);
 
     const nameChangeHandler = event => {
         setEnteredName(event.target.value);
+        if(enteredName.trim().length === 0) {
+          setNameIsValid(false)  
+        } else {
+            setNameIsValid(true);
+        } 
     }
 
     const ageChangeHandler = event => {
         setAge(event.target.value);
+        enteredAge.trim().length === 0 ? setAgeIsValid(false) : setAgeIsValid(true);
     }
 
     const submitHandler = event => {
@@ -27,11 +35,13 @@ const UserInput = props => {
     }
 
     return (
-        <Card className="user-form">
+        <Card className={styles['user-form']}>
             <form onSubmit={submitHandler}>
-                <div className="form-control">
+                <div className={`${styles['form-control']} ${!nameIsValid && styles.invalid}`}>
                     <label>Name</label>
                     <input type="text" onChange={nameChangeHandler}/>
+                </div>
+                <div className={`${styles['form-control']} ${!ageIsValid && styles.invalid}`}>
                     <label>Age</label>
                     <input type="number" onChange={ageChangeHandler} />
                 </div>
