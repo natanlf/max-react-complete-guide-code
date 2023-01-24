8d1a3f85da20e5957f050c0939a6e1dbe81ecf01
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-01-24 19:19:24 -0300

    side effects and useEffect

--- code/reducers/src/App.js
+++ code/reducers/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  /* When the state change, the react function rerun, because this is important useEffect, prevent this */
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storeUserLoggedIn = localStorage.getItem('isLoggedIn');
+    console.log('UseEffect')
+    if(storeUserLoggedIn === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []) //The second parameter is for dependencies, you can put some dependencie there and the useEfecct will run when o state of dependencie change
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
