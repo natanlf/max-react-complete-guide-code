7d93676ddb373f678fb35452ad71c9fefb017e4c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-04-03 20:20:19 -0300

    react context api

--- code/reducers/src/App.js
+++ code/reducers/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   /* When the state change, the react function rerun, because this is important useEffect, prevent this */
@@ -29,13 +30,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
