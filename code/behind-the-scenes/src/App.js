23e87edb0f8948e1d51d5d4153fa942fef3f5edd
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-04-10 20:30:17 -0300

    useCallback dependencies

--- code/behind-the-scenes/src/App.js
+++ code/behind-the-scenes/src/App.js
@@ -7,6 +7,7 @@ import DemoOutput from './components/demo/DemoOutput';
 function App() {
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
@@ -15,16 +16,24 @@ function App() {
     Without this, the button component is always re-executed because the comparasion is with function and this is object, 
     if I create the function every time, the comparasion will be different, previous and current state is diferent.
     Something like this: obj1 === obj2
+    if allowToggle changes then the function is re-created because of the dependency informed in hook useCallback.
+    Because of this we always using the current state of allowToggle
   */
   const toogleParagraphHandler = useCallback(() => {
-    setShowParagraph( (prevState) => !prevState );
-  },[]);
+    if(allowToggle) {
+      setShowParagraph( (prevState) => !prevState );
+    }
+  },[allowToggle]);
 
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toogleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
